4a2f15c273bf28cc3a9f8fbfe1bc1812fa86b7f5
Stein M. Nornes <stein@nlink.no>
2022-03-08 12:58:53 +0100

    Attempt to reconnect after connection loss

--- scripts/modbus_device_driver.py
+++ scripts/modbus_device_driver.py
@@ -7,6 +7,7 @@ import logging
 import rospy
 
 from ros_modbus_device import *
+from pymodbus.exceptions import ConnectionException
 
 
 logger = logging.getLogger(__name__)
@@ -23,9 +24,15 @@ rospy.init_node('modbus_device_driver', anonymous=True)
 modbus_device = ROSModbusSlaveDevice()
 
 logger.info("connecting to: %s:%d", modbus_device.config.config['address'], modbus_device.config.config['port'])
-while not modbus_device.connect() and not rospy.is_shutdown():
-    rospy.sleep(1)
-    logger.info("Retrying connecting to: %s:%d", modbus_device.config.config['address'], modbus_device.config.config['port'])
-    
-if not rospy.is_shutdown():
-    modbus_device.run()
+while not rospy.is_shutdown():
+    while not modbus_device.connect() and not rospy.is_shutdown():
+        rospy.sleep(1)
+        logger.info("Retrying connecting to: %s:%d", modbus_device.config.config['address'], modbus_device.config.config['port'])
+        
+    if not rospy.is_shutdown():
+        try:
+            modbus_device.run()
+        except ConnectionException as err:
+            logger.error("Lost connection! Error message: {}".format(err))
+            rospy.sleep(1)
+            logger.info("Attempting to reestablish connection to: %s:%d", modbus_device.config.config['address'], modbus_device.config.config['port'])
